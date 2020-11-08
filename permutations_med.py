57e4f74b260a59e77f77c76e190bac14b5518c88
Ricky <rickdhanota@gmail.com>
2020-11-08 00:39:55 -0800

    added permutations

--- permutations_med.py
+++ permutations_med.py
@@ -6,4 +6,20 @@
 #sample output; [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 2, 1], [3, 2, 1]]
 
 def getPermutations(array):
-    pass
+    permutations = []
+    permutationsHelper(0, array, permutations)
+    return permutations
+
+def permutationsHelper(i, array, permutations):
+    if i == len(array) - 1:
+        permutations.append(array[:])
+    else:
+        for j in range(i, len(array)):
+            swap(array, i, j)
+            permutationsHelper(i+1, array, permutations)
+            swap(array, i, j)
+
+def swap(array, i, j):
+    array[i], array[j] = array[j], array[i]
+
+print(getPermutations([1, 2, 3]))
