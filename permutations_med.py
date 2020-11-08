53f82f421490cdb404febbc170339bf8d702ffd2
Ricky <rickdhanota@gmail.com>
2020-11-08 00:33:00 -0800

    added stuff

---
+++ permutations_med.py
@@ -0,0 +1,9 @@
+#Permutation
+#write a function that takes in an array of unique integers and returns an array of all permuations of those integers in no particular order
+#If the input array is empty, the function should return an empty array
+
+#array = [1, 2, 3]
+#sample output; [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 2, 1], [3, 2, 1]]
+
+def getPermutations(array):
+    pass
